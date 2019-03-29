65bd1311d60ea7384651726e057315e66df8fa79
Sun <geng.sun@sage.com>
2019-03-29 10:35:14 +0000

    fixed body

--- add-labels.sh
+++ add-labels.sh
@@ -11,18 +11,16 @@ URI=https://api.github.com
 API_VERSION=v3
 API_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+BODY='{"labels":["squad whiskey", "Pending QA", "Pending Review"]}'
 
-# pr_number=$(jq --raw-output .pull_request.id "$GITHUB_EVENT_PATH")
-# owner=$(jq --raw-output .pull_request.head.repo.owner.login "$GITHUB_EVENT_PATH")
-# repo=$(jq --raw-output .pull_request.head.repo.name "$GITHUB_EVENT_PATH")
 issue_url=$(jq --raw-output .pull_request.issue_url "$GITHUB_EVENT_PATH")
 
-echo "${URI}/repos/${owner}/${repo}/issues/${pr_number}/labels"
+echo $*
 
 curl -XPOST -fsSL \
 	-H "${AUTH_HEADER}" \
 	-H "${API_HEADER}" \
-	-d "[$*]" \
+	-d "${BODY}" \
 	"${issue_url}"
 
 exit 0
