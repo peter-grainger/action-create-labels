b8cae63f45d6ed8e044811128269ec207adcc0b5
Peter Grainger <peter.grainger@sage.com>
2019-03-29 09:27:46 +0000

    wip

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM alpine:3.9
+LABEL maintainer="Peter Grainger<peter@grainger.xyz>"
+
+LABEL "com.github.actions.name"="Add labels to PR"
+LABEL "com.github.actions.description"="Add a label to a PR.  Pre-defined colours for labels\n 'whiskey squad', 'pending review', 'pending qa'"
+LABEL "com.github.actions.icon"="activity"
+LABEL "com.github.actions.color"="red"
+
+RUN	apk add --no-cache \
+  bash \
+  ca-certificates \
+  curl \
+  jq
+
+COPY add-labels.sh /usr/bin/add-labels.sh
+
+ENTRYPOINT ["add-labels.sh"]
---
+++ add-labels.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "Set the GITHUB_TOKEN env variable."
+	exit 1
+fi
+
+URI=https://api.github.com
+API_VERSION=v3
+API_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
+AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
+
+pr_number=$(jq --raw-output .pull_request.id "$GITHUB_EVENT_PATH")
+owner=$(jq --raw-output .pull_request.head.repo.owner.login "$GITHUB_EVENT_PATH")
+repo=$(jq --raw-output .pull_request.head.repo.name "$GITHUB_EVENT_PATH")
+
+curl -XGET -fsSL \
+	-H "${AUTH_HEADER}" \
+	-H "${API_HEADER}" \
+	-d "[$*]"
+	"${URI}/repos/${owner}/${repo}/issues/${pr_number}/labels"
+
+exit 0
