64d9bc340ce82c4fa93d556d8698a3a3b00c9179
Sun <geng.sun@sage.com>
2019-03-29 10:15:15 +0000

    added issue_url

--- add-labels.sh
+++ add-labels.sh
@@ -12,9 +12,10 @@ API_VERSION=v3
 API_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 
-pr_number=$(jq --raw-output .pull_request.id "$GITHUB_EVENT_PATH")
-owner=$(jq --raw-output .pull_request.head.repo.owner.login "$GITHUB_EVENT_PATH")
-repo=$(jq --raw-output .pull_request.head.repo.name "$GITHUB_EVENT_PATH")
+# pr_number=$(jq --raw-output .pull_request.id "$GITHUB_EVENT_PATH")
+# owner=$(jq --raw-output .pull_request.head.repo.owner.login "$GITHUB_EVENT_PATH")
+# repo=$(jq --raw-output .pull_request.head.repo.name "$GITHUB_EVENT_PATH")
+issue_url=$(jq --raw-output .pull_request.issue_url "$GITHUB_EVENT_PATH")
 
 echo "${URI}/repos/${owner}/${repo}/issues/${pr_number}/labels"
 
@@ -22,6 +23,6 @@ curl -XPOST -fsSL \
 	-H "${AUTH_HEADER}" \
 	-H "${API_HEADER}" \
 	-d "[$*]" \
-	"${URI}/repos/${owner}/${repo}/issues/${pr_number}/labels"
+	"${issue_url}"
 
 exit 0
